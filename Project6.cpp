c7a95f101662d5eb5b9850877d5741b14c0b0d4a
DedaEga <Egor.Boiarshinov@urfu.me>
2022-11-30 21:07:49 +0500

    Добавьте файлы проекта.

---
+++ Project6.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int count;
+	cout << "Vvedite kolichestvo kriteriev: ";
+	cin >> count;
+	double** nums = new double* [count];
+	for (int i = 0; i < count; i++) {
+		nums[i] = new double[count];
+	}
+	for (int i = 0; i < count; i++) {
+		for (int j = 0; j < count; j++) {
+			if (i == j) {
+				nums[i][j] = 1;
+			}
+			else if(i < j) {
+				while (true) {
+					cout << "Vvedite koefficient sravneniya " << i + 1 << " s " << j + 1 << "(ot 0 do 9): ";
+					cin >> nums[i][j];
+					if ((nums[i][j] > 9) or (nums[i][j] < 0)) {
+						cout << "Nedopustimoe znachenie, poprobujte eshchyo raz\n";
+					}
+					else {
+						break;
+					}
+				}
+			}
+		}
+	}
+}
